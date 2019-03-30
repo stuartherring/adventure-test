13fd9606f9689645632520e3011da585d9aa0e63
stuartherring <stuart.herring@domain.com.au>
2019-03-31 09:15:44 +1100

    Made Changes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import './styles.css';
 const App = () => (
   <div className="App">
     <h1>Adventure Test</h1>
-    <h2>Start editing to see some magic happen!</h2>
+    <h2>Do stuff</h2>
   </div>
 );
 
